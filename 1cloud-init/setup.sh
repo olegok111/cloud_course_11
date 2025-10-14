1e6128e1e6d71b5c66760ffb31055d2ff2f6173c
olegok111 <olegokoyomov2@gmail.com>
2025-10-14 13:01:54 +0300

    [cloud-init] enhance script

--- 1cloud-init/setup.sh
+++ 1cloud-init/setup.sh
@@ -1,9 +1,9 @@
-ssh-keygen -f id_rsa -t rsa
-sed -i "s/SSHPUBKEY/$(cat id_rsa.pub)/" user-data.yaml
+[ ! -f id_rsa ] && ssh-keygen -f id_rsa -t rsa
+awk '{ getline key < "id_rsa.pub"; gsub("SSHPUBKEY", key); print > "user-data.yaml" }' user-data.yaml.in
 cloud-localds cloud-seed.img user-data.yaml meta-data.yaml
-sudo chown ${USER}:libvirt-qemu cloud-seed.img
 sudo cp cloud-seed.img /var/lib/libvirt/images/
+sudo chown ${USER}:libvirt-qemu /var/lib/libvirt/images/cloud-seed.img
 [ ! -f alt-p11-cloud-x86_64.qcow2 ] && wget https://ftp.altlinux.org/pub/distributions/ALTLinux/p11/images/cloud/x86_64/alt-p11-cloud-x86_64.qcow2
-sudo chown ${USER}:libvirt-qemu alt-p11-cloud-x86_64.qcow2 
 sudo cp alt-p11-cloud-x86_64.qcow2 /var/lib/libvirt/images/
+sudo chown ${USER}:libvirt-qemu /var/lib/libvirt/images/alt-p11-cloud-x86_64.qcow2 
 sudo virsh net-define net.xml
