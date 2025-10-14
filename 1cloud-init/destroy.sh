f0ebfdb637b239727e9479cdbca7bf07c693d0b0
olegok111 <olegokoyomov2@gmail.com>
2025-10-14 16:49:21 +0300

    [cloud-init] Task 1 DONE

---
+++ 1cloud-init/destroy.sh
@@ -0,0 +1,3 @@
+rm cloud-seed.img user-data.yaml
+sudo virsh destroy 11cloud-1
+sudo virsh undefine 11cloud-1
--- 1cloud-init/setup.sh
+++ 1cloud-init/setup.sh
@@ -6,4 +6,12 @@ sudo chown ${USER}:libvirt-qemu /var/lib/libvirt/images/cloud-seed.img
 [ ! -f alt-p11-cloud-x86_64.qcow2 ] && wget https://ftp.altlinux.org/pub/distributions/ALTLinux/p11/images/cloud/x86_64/alt-p11-cloud-x86_64.qcow2
 sudo cp alt-p11-cloud-x86_64.qcow2 /var/lib/libvirt/images/
 sudo chown ${USER}:libvirt-qemu /var/lib/libvirt/images/alt-p11-cloud-x86_64.qcow2 
-sudo virsh net-define net.xml
+sudo virsh net-create net.xml
+sudo virt-install --name 11cloud-1 \
+    --ram 2048 --vcpus 2 \
+    --disk path=/var/lib/libvirt/images/alt-p11-cloud-x86_64.qcow2 \
+    --disk path=/var/lib/libvirt/images/cloud-seed.img,format=raw \
+    --os-variant linux2024 \
+    --virt-type kvm \
+    --network network=cl,model=virtio \
+    --import
